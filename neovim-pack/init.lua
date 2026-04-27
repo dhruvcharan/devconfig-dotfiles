112096ca22c74d2d38318023438120d71c9cf553
dhruvcharan222 <dhruvcharan222@gmail.com>
2026-04-26 19:29:28 -0600

    Added Updates to init.lua file

--- neovim-pack/init.lua
+++ neovim-pack/init.lua
@@ -16,21 +16,23 @@ end
 local function bootstrap_and_load(plugins)
   local pack_path = vim.fn.stdpath('config') .. '/pack/plugins/start'
   vim.fn.mkdir(pack_path, "p")
-  
+
   for name, spec in pairs(plugins) do
     local repo = type(spec) == 'string' and spec or spec.repo
     local branch = type(spec) == 'table' and spec.branch or nil
     local install_path = pack_path .. '/' .. name
-    
+
     if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
       print('Installing plugin: ' .. name)
       local cmd = { 'git', 'clone', '--depth', '1' }
-      if branch then table.insert(cmd, '--branch'); table.insert(cmd, branch) end
+      if branch then
+        table.insert(cmd, '--branch'); table.insert(cmd, branch)
+      end
       table.insert(cmd, 'https://github.com/' .. repo)
       table.insert(cmd, install_path)
       vim.fn.system(cmd)
     end
-    
+
     -- CRITICAL: Add to runtimepath so we can 'require' them in this file
     vim.opt.rtp:prepend(install_path)
   end
@@ -39,7 +41,6 @@ end
 -- 5. Load Plugin List and Load them into RTP
 local plugins_module = require('plugins.init')
 bootstrap_and_load(plugins_module.plugins)
-
 -- 6. Plugin Setup
 plugins_module.setup()
 
